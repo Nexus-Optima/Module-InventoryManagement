8dac638686975bd0e6c69bc53d332d5bd6695f5d
kaivan <kaivanshah1663@gmail.com>
2023-12-17 23:36:08 +0530

    Using AWS to send emails

--- Utils/send_email.py
+++ Utils/send_email.py
@@ -1,22 +1,62 @@
-import smtplib
+import boto3
+from botocore.exceptions import ClientError
 import os
-from email.mime.text import MIMEText
 
 
 def send_email(name, email, message):
     sender_email = os.getenv("SENDER_EMAIL_ADDRESS")
-    sender_password = os.getenv("SENDER_EMAIL_PASSWORD")
     receiver_email = os.getenv("RECEIVER_EMAIL_ADDRESS")
+    aws_region = os.getenv("AWS_REGION")
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY_ID")
 
-    print(sender_email, sender_password, receiver_email)
+    client = boto3.client(
+        'ses',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=aws_region
+    )
 
-    msg = MIMEText(f"Name: {name}\nEmail: {email}\nMessage: {message}")
-    msg['Subject'] = "New Contact Form Message"
-    msg['From'] = sender_email
-    msg['To'] = receiver_email
+    subject = "New Contact Form Message"
+    body_text = f"Name: {name}\nEmail: {email}\nMessage: {message}"
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(sender_email, sender_password)
-    server.sendmail(sender_email, receiver_email, msg.as_string())
-    server.quit()
+    body_html = f"""<html>
+    <head></head>
+    <body>
+      <h1>New Contact Form Message</h1>
+      <p>Name: {name}<br>
+         Email: {email}<br>
+         Message: {message}</p>
+    </body>
+    </html>
+                """
+    try:
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    receiver_email,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': "UTF-8",
+                        'Data': body_html,
+                    },
+                    'Text': {
+                        'Charset': "UTF-8",
+                        'Data': body_text,
+                    },
+                },
+                'Subject': {
+                    'Charset': "UTF-8",
+                    'Data': subject,
+                },
+            },
+            Source=sender_email,
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
