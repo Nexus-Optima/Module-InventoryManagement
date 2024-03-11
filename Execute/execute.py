e25c6ccdad237fbf1b178b76cfced7d3cfb3f577
unknown <abhilakshmisra@gmail.com>
2024-03-11 10:28:19 +0530

    updated files

--- Execute/execute.py
+++ Execute/execute.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import requests
+import boto3
+import io
 from pathlib import Path
-
 import Constants.constants as cts
 from Calculate.calculate_action_plan import calculate_action_plan_and_priority
-
 
 def execute():
     data = read_data()
@@ -14,21 +13,31 @@ def execute():
         data.at[i, cts.Columns.PRIORITY] = prio
     return data
 
+def post_data(data):
+    csv_data = data.to_csv(index=False)  
+    s3 = boto3.client('s3')
+    bucket_name = 'b3llcurve-inventory-management'
+    folder_name = 'data/'  
+    file_name = 'data.csv' 
+    s3.put_object(Bucket=bucket_name, Key=folder_name + file_name, Body=csv_data)
 
+def fetch_data():
+    try:
+        s3 = boto3.client('s3')
+        bucket_name = 'b3llcurve-inventory-management'
+        folder_name = 'data/'  
+        file_name = 'data.csv' 
+        response=s3.get_object(Bucket=bucket_name, Key=folder_name + file_name)
+        csv_data = response['Body'].read().decode('utf-8')
+        df = pd.read_csv(io.StringIO(csv_data))
+        return df
+    except Exception as e:
+        return {"error": str(e)}
+    
 def read_data():
     current_dir = Path(__file__).parent
     csv_path = current_dir / '..' / 'Data' / 'Module2.csv'
     data = pd.read_csv(csv_path)
     return data
-
-
-def fetch_data(url, params):
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        print(data)
-    else:
-        print(f"Failed to fetch data. Status code: {response.status_code}")
-
 
 execute()
